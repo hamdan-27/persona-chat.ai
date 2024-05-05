0cf73d79aa10d6b4f6c2fee0472dbe691bff671c
hamdan-27 <hamdan102703@gmail.com>
2024-05-06 02:52:11 +0400

    add queries back to each endpoint

--- app/admin.py
+++ app/admin.py
@@ -5,19 +5,22 @@ from models import User, Persona, Data
 from config import UploadFileForm, db, base_dir, app
 import os
 
-admin = Blueprint('admin', __name__, static_folder='static', template_folder='templates/admin')
+admin = Blueprint('admin', __name__, static_folder='static',
+                  template_folder='templates/admin')
+
 
 @admin.route('/')
 @admin.route('/dashboard')
 @login_required
 def dashboard():
     if current_user.is_admin:
-        users = User.query.filter_by(is_admin=False).all()
-        personas = Persona.query.all()
-        data = Data.query.all()
+        user_list = User.query.filter_by(is_admin=False).all()
+        all_personas = Persona.query.all()
+        all_data = Data.query.all()
+        admins = User.query.filter_by(is_admin=True).all()
         return render_template(
-            'dashboard.html', users=users[:5], user_count=len(users),
-            persona_count=len(personas), data_count=len(data))
+            'dashboard.html', users=user_list[:5], user_count=len(user_list),
+            persona_count=len(all_personas), data_count=len(all_data), admin_count=len(admins))
     else:
         abort(401)
 
@@ -36,34 +39,36 @@ def users():
                     username = request.form.get('username')
                     oldpass = request.form.get('old-password')
                     newpass = request.form.get('new-password')
-                
+
                     user.name = name
                     user.email = email
                     user.username = username
 
                     if oldpass and newpass:
+                        print('oldpass', oldpass)
+                        print('newpass', newpass)
                         if user.check_password(oldpass):
                             user.set_password(newpass)
                         else:
                             flash('Invalid current password entered', 'error')
                             return redirect(url_for('admin.users'))
-                    
+
                     db.session.commit()
                     flash(f'User {user.id} updated successfully', 'success')
                     return redirect(url_for('admin.users'))
-                
+
                 elif 'delete-user' in request.form:
                     db.session.delete(user)
                     db.session.commit()
                     flash(f'User {user.id} deleted successfully', 'success')
                     return redirect(url_for('admin.users'))
-                
+
             else:
                 flash('User not found', 'error')
                 return redirect(url_for('admin.users'))
-                
-        users = User.query.filter_by(is_admin=False).all()
-        return render_template('users.html', users=users, count=len(users))
+            
+        user_list = User.query.filter_by(is_admin=False).all()
+        return render_template('users.html', users=user_list, count=len(user_list))
     else:
         abort(401)
 
@@ -81,10 +86,13 @@ def data():
             if data:
                 if 'update-data' in request.form:
                     filename = request.form.get('filepath')
-                    new_filepath = secure_filename(filename + '.' + data.datatype)
+                    new_filepath = secure_filename(
+                        filename + '.' + data.datatype)
                     os.rename(
-                        os.path.join(base_dir, app.config['UPLOAD_FOLDER'], data.filepath),
-                        os.path.join(base_dir, app.config['UPLOAD_FOLDER'], new_filepath)
+                        os.path.join(
+                            base_dir, app.config['UPLOAD_FOLDER'], data.filepath),
+                        os.path.join(
+                            base_dir, app.config['UPLOAD_FOLDER'], new_filepath)
                     )
                     data.filepath = new_filepath
                     db.session.commit()
@@ -92,24 +100,49 @@ def data():
                     return redirect(url_for('admin.data'))
                 elif 'delete-data' in request.form:
                     db.session.delete(data)
-                    os.remove(os.path.join(base_dir, app.config['UPLOAD_FOLDER'], data.filepath))
+                    os.remove(os.path.join(
+                        base_dir, app.config['UPLOAD_FOLDER'], data.filepath))
                     db.session.commit()
                     flash(f'Data {data._id} deleted successfully', 'success')
                     return redirect(url_for('admin.data'))
                 
+        user_list = User.query.filter_by(is_admin=False).all()
         all_data = Data.query.all()
-        users = User.query.filter_by(is_admin=False).all()
-        return render_template('admin_data.html', files=all_data, count=len(all_data), form=form, users=users)
+        return render_template('admin_data.html', files=all_data, 
+                               count=len(all_data), form=form, users=user_list)
     else:
         abort(401)
-
 
 
 @admin.route('/personas', methods=['GET', 'POST'])
 @login_required
 def personas():
     if current_user.is_admin:
-        return render_template('admin_personas.html')
+        if request.method == 'POST':
+            id = request.form.get('id')
+            persona = Persona.query.get(id)
+            if persona:
+                if 'update-persona' in request.form:
+                    name = request.form.get('update-name')
+                    prompt = request.form.get('update-prompt')
+                    data_id = request.form.get('data-id')
+                    persona.name = name
+                    persona.prompt = prompt
+                    persona.data_id = data_id
+                    db.session.commit()
+                    flash(f'Persona {persona._id} updated successfully', 'success')
+                    return redirect(url_for('admin.personas'))
+                elif 'delete-persona' in request.form:
+                    db.session.delete(persona)
+                    db.session.commit()
+                    flash(f'Persona {persona._id} deleted successfully', 'success')
+                    return redirect(url_for('admin.personas'))
+                
+        user_list = User.query.filter_by(is_admin=False).all()
+        all_data = Data.query.all()
+        all_personas = Persona.query.all()
+        return render_template('admin_personas.html', data_list=all_data, 
+                               personas=all_personas, count=len(all_personas), users=user_list)
     else:
         abort(401)
 
@@ -118,6 +151,7 @@ def personas():
 @login_required
 def account():
     if current_user.is_admin:
-        return render_template('account.html')
+        return render_template('account.html', fullname=session['name'],
+                               email=session['email'])
     else:
         abort(401)
